8ea78f8358d1ffee7c4a4242e65051f84bfcea7b
Sebastian Aguirre <osebas15@gmail.com>
2024-07-07 10:49:42 -0400

    Initial Commit

---
+++ Tyme Watch App/TymeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TymeApp.swift
+//  Tyme Watch App
+//
+//  Created by Sebastian Aguirre on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Tyme_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tyme/TymeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TymeApp.swift
+//  Tyme
+//
+//  Created by Sebastian Aguirre on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TymeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
