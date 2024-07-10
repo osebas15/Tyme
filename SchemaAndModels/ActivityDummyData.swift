8cc4917d682318c9d4d74afafb7df25c9db7c45d
Sebastian Aguirre <osebas15@gmail.com>
2024-07-10 16:50:26 -0400

    make dummydata factory funcs static

--- SchemaAndModels/ActivityDummyData.swift
+++ SchemaAndModels/ActivityDummyData.swift
@@ -9,7 +9,7 @@ import Foundation
 
 
 extension Activity {
-    func getDummyActivities() -> [Activity] {
+    static func getDummyActivities() -> [Activity] {
         // Create dummy data
         let activity1 = Activity()
         activity1.name = "parent"
@@ -27,19 +27,19 @@ extension Activity {
         activity3.detail = "Detail of Activity 3"
         activity3.timeToComplete = 2700 // 45 minutes
         activity3.onOffTimes?.append(TimeRange(start: Date(), end: Date().addingTimeInterval(30 * 60)))
-        
+        /*
         let activity4 = Activity()
-        activity3.name = "always"
-        activity3.detail = "but part of parent"
-        activity3.timeToComplete = 60 * 5 // 5 minutes
-        activity3.onOffTimes?.append(contentsOf: [
+        activity4.name = "always"
+        activity4.detail = "but part of parent"
+        activity4.timeToComplete = 60 * 5 // 5 minutes
+        activity4.onOffTimes?.append(contentsOf: [
             TimeRange(
                 start: Date(), 
                 end: Date().addingTimeInterval(60)),
             TimeRange(
                 start: Date().addingTimeInterval(4 * 60),
                 end: Date().addingTimeInterval(5 * 60))
-        ])
+        ])*/
 
         // Link the activities
         activity1.next = activity2
@@ -55,7 +55,7 @@ extension Activity {
         return [activity1, activity2, activity3]
     }
     
-    func swissBurgerRecepie() -> [Activity]{
+    static func swissBurgerRecepie() -> [Activity]{
         // Create dummy data for making a Swiss burger activity
         let swissBurger = Activity()
         swissBurger.name = "Make Swiss Burger"
