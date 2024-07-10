6f92b237596ee9ff9d8e0988073daa0861f8e9bc
Sebastian Aguirre <osebas15@gmail.com>
2024-07-10 16:51:54 -0400

    create or load preexisting container

--- SchemaAndModels/ModelHelper.swift
+++ SchemaAndModels/ModelHelper.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import SwiftData
+
+
+struct ModelHelper {
+    static func getBasicContainer() -> ModelContainer {
+        let schema = Schema([
+            Activity.self
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        var modelContainer: ModelContainer?
+        do {
+            modelContainer = try ModelContainer(
+                for: schema,
+                configurations: [modelConfiguration]
+            )
+
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+        guard let modelContainer = modelContainer else {
+            fatalError("Could not create ModelContainer")
+        }
+        
+        return modelContainer
+    }
+    
+    static func getTestContainer() -> ModelContainer {
+        return getBasicContainer()
+    }
+}
