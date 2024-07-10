a4e0759d22e71492f4b719bcec081f132efc9bc1
Sebastian Aguirre <osebas15@gmail.com>
2024-07-10 15:32:05 -0400

    create View files and add activity model to the watch app

---
+++ Tyme Watch App/ActiveScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ActiveScreen.swift
+//  Tyme Watch App
+//
+//  Created by Sebastian Aguirre on 7/10/24.
+//
+
+import SwiftUI
+
+struct ActiveScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActiveScreen()
+}
---
+++ Tyme Watch App/HomeView.swift
@@ -0,0 +1,25 @@
+//
+//  HomeView.swift
+//  Tyme Watch App
+//
+//  Created by Sebastian Aguirre on 7/10/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    //Query with swiftdata use
+    var activities: [Activity]
+    var body: some View {
+        VStack{
+            Text("Start")
+            List(){
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
