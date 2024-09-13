12e023d1d0007b32dbf454182254c223de98bb61
Sebastian Aguirre <osebas15@gmail.com>
2024-09-13 15:48:00 -0400

    start HomeView for iphone and Activitiy View for AppleWatch

---
+++ Tyme Watch App/HomeView.swift
@@ -0,0 +1,25 @@
+//
+//  HomeView.swift
+//  Tyme Watch App
+//
+//  Created by Sebastian Aguirre on 7/31/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct HomeView: View {
+    @Environment(\.modelContext) private var context
+    
+    @Query(filter: #Predicate<ActivityClass>{ act in
+        act.parent == nil
+    }) var activityClasses: [ActivityClass]
+    
+    var body: some View {
+        Text("Hello, World! \(activityClasses.count) \(context)")
+    }
+}
+
+#Preview {
+    HomeView()
+}
