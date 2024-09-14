2df7c2f7f1943385d9ffd0c357e696604f0734ef
Sebastian Aguirre <osebas15@gmail.com>
2024-09-14 17:43:41 -0400

    create ActivityCellFull

---
+++ Tyme/ActivityCellFull.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityCellFull.swift
+//  Tyme
+//
+//  Created by Sebastian Aguirre on 9/14/24.
+//
+
+import SwiftUI
+
+struct ActivityCellFull: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityCellFull()
+}
