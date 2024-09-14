08e2ab0b741d1d9d8c3c872bd72ac39fd0ae25a8
Sebastian Aguirre <osebas15@gmail.com>
2024-09-14 17:44:50 -0400

    show basic activity information

--- Tyme/ActivityCellFull.swift
+++ Tyme/ActivityCellFull.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct ActivityCellFull: View {
+    var activity: ActivityObject
+    
+    var totalTime: TimeInterval?{
+        get {
+            return activity.onOffTimes?
+                .reduce(TimeInterval(0), {$0 + $1.totalTime()})
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text(activity.activityClass.name ?? "name error")
+            HStack{
+                Text(totalTime?.description ?? "time error")
+            }
+        }
     }
 }
 
