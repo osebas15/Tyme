b6a0c944466a852e2faf2eef535871cda8012680
Sebastian Aguirre <osebas15@gmail.com>
2024-10-29 23:48:15 -0400

    usable watch UI to move through recepie

--- Tyme Watch App/ActiveView.swift
+++ Tyme Watch App/ActiveView.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ActiveView: View {
+    @Environment(\.modelContext) var context: ModelContext
     var activities: [ActivityObject]
+    
     var body: some View {
-        List(activities){ activity in
-            Text(activity.activityClass!.name)
+        if activities.count == 1 && activities[0].subActivities.count > 0 {
+            ActiveView(activities: activities[0].subActivities)
+        }
+        else {
+            List(activities){ activity in
+                Text(activity.activityClass!.name)
+                    .onTapGesture {
+                        activity.done(context: context)
+                    }
+            }
         }
     }
 }
